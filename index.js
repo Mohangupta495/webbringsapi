07e95ade7173f6bbfaa3faf14ef34013db383b40
Mohan Gupta <75502789+Mohangupta495@users.noreply.github.com>
2023-02-26 23:57:21 +0530

    uncomment db

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 // const dotenv = require("dotenv");
-// const connectionDB = require("./db");
+const connectionDB = require("./db");
 // const cors = require("cors");
 
 const app = express();
 // app.use(cors());
-// connectionDB();
+connectionDB();
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
