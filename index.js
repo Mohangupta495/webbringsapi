06ba5dc837a90c4e134b5caa0d18f8ac204f1c48
Mohan Gupta <75502789+Mohangupta495@users.noreply.github.com>
2023-02-26 23:56:24 +0530

    commit all

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const connectionDB = require("./db");
-const cors = require("cors");
+// const dotenv = require("dotenv");
+// const connectionDB = require("./db");
+// const cors = require("cors");
 
 const app = express();
 // app.use(cors());
 // connectionDB();
-app.get("/", (req, res) => {
-  res.send("Welcome to WebBrings api section.");
-});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+app.get("/", (req, res) => {
+  res.send("Welcome to WebBrings api section.");
 });
